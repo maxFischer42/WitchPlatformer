89bd2ab256c20ad2880eac869438e8c880bb5bd8
maxFischer42 <42ducksable1@gmail.com>
2018-10-05 06:41:37 -0700

    Placeholder data

---
+++ Assets/Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float jumpAccel;
+    private Rigidbody2D rigid;
+    public int groundLayer;
+    public bool midair;
+    public GameObject feetObject;
+
+
+
+
+
+
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update ()
+    {
+        Vector2 currentSpeed = Vector2.zero;
+        if (midair)
+            return;
+        float x = GetX();
+        x *= horizontalSpeed;
+        float y = GetJump();
+        y *= jumpAccel;
+        if(x != 0)
+        {
+            currentSpeed = new Vector2(x, 0f);
+        }
+        if(y != 0)
+        {
+            currentSpeed = new Vector2(currentSpeed.x, y);
+        }
+        rigid.velocity = currentSpeed;
+	}
+
+    float GetX()
+    {
+        float x = Input.GetAxis("Horizontal");
+        return x;
+    }
+
+    public void isGrounded(bool value)
+    {
+        midair = !value;
+    }
+
+    float GetJump()
+    {
+        float y = Input.GetAxis("Jump");
+        return y;
+    }
+
+}
---
+++ Assets/feetDetection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feetDetection : MonoBehaviour
+{
+
+    private Controller player;
+    private int layer;
+
+    void Start()
+    {
+        player = transform.parent.GetComponent<Controller>();
+        layer = player.groundLayer;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(layer == collision.gameObject.layer)
+        {
+            player.isGrounded(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (layer == collision.gameObject.layer)
+        {
+            player.isGrounded(false);
+        }
+    }
+
+
+}
