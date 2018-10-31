cc590b4cc739386a971fc9a18572f3a73d15b8b5
Max Fischer <42ducksable1@gmail.com>
2018-10-30 18:37:47 -0700

    Skeleman

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Ladder : MonoBehaviour {
 
-    public bool lockInput = false;
     public float climbVelocity = 1f;
     public int layer;
     private Rigidbody2D playerRigidbody;
     private PlatformController _platform;
     private bool isColliding;
     private float gScale;
+    private GameObject player;
+    private GameObject collidingObject;
 
 	// Use this for initialization
 	void Start () {
         _platform = GameObject.FindObjectOfType<PlatformController>().GetComponent<PlatformController>();
         playerRigidbody = GameObject.FindObjectOfType<PlatformController>().GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").gameObject;
+        
 	}
 	
 	// Update is called once per frame
@@ -26,6 +29,16 @@ public class Ladder : MonoBehaviour {
             if(Input.GetButtonDown("Jump"))
             {
                 isColliding = false;
+                if (collidingObject.gameObject.layer == layer)
+                {
+                    
+                    collidingObject.GetComponent<Rigidbody2D>().gravityScale = gScale;
+                    isColliding = false;
+                    player.GetComponent<PlatformController>().enabled = true;
+                    player.GetComponent<PlatformController>().Grounded = false;
+                    player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, player.GetComponent<PlatformController>().jumpAcceleration));
+                    collidingObject = null;
+                }
                 return;
             }
             float y = Input.GetAxisRaw("Vertical");
@@ -40,17 +53,25 @@ public class Ladder : MonoBehaviour {
         if(collision.gameObject.layer == layer)
         {
             isColliding = true;
+            collidingObject = collision.gameObject;
             gScale = collision.GetComponent<Rigidbody2D>().gravityScale;
             collision.GetComponent<Rigidbody2D>().gravityScale = 0;
+            player.GetComponent<PlatformController>().enabled = false;
         }
     }
+
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.layer == layer)
         {
+            
             collision.GetComponent<Rigidbody2D>().gravityScale = gScale;
             isColliding = false;
+            player.GetComponent<PlatformController>().enabled = true;
+            player.GetComponent<PlatformController>().Grounded = false;
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,player.GetComponent<PlatformController>().jumpAcceleration));
+            collidingObject = null;
         }
     }
 
