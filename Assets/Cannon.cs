e3f5b6bd675fd8dba865727c15403f5c0a93bc1b
Max Fischer <42ducksable1@gmail.com>
2018-11-19 19:46:54 -0800

    cannonballs

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -9,7 +9,9 @@ public class Cannon : MonoBehaviour {
     public float rotationSpeed;
     [Range(1, 600)]
     public float projectileSpawnTimer;
+    public float force;
     public GameObject projectile;
+    public Transform spawn;
     private Rigidbody2D m_rigidbody;
 
     private float hiddenTimer = 0f;
@@ -22,14 +24,24 @@ public class Cannon : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        projectileSpawnTimer += Time.deltaTime;
         Rotation();
-
-
+        if(hiddenTimer >= projectileSpawnTimer)
+        {
+            hiddenTimer = 0f;
+            GameObject obj = projectile;
+            Vector2 direction = playerObject.position - transform.position;
+            direction *= force;
+            direction = new Vector2(direction.x, Mathf.Abs(direction.y * 25f));
+            obj.GetComponent<Rigidbody2D>().velocity = direction;
+            GameObject newObj = (GameObject)Instantiate(obj,spawn);
+            newObj.transform.parent = null;
+            Destroy(newObj, 4f);
+        }
     }
 
     void Rotation()
     {
+        hiddenTimer += Time.deltaTime;
         Vector2 direction = playerObject.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         if (angle > 240 || angle < -60 || (angle > 240 && angle < 300))
