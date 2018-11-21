300b42c099b836a5f78a67fa51cd28d27c726bc8
Max Fischer <42ducksable1@gmail.com>
2018-11-20 22:42:00 -0800

    more cannon stuff

--- Assets/Cannon.cs
+++ Assets/Cannon.cs
@@ -10,6 +10,7 @@ public class Cannon : MonoBehaviour {
     [Range(1, 600)]
     public float projectileSpawnTimer;
     public float force;
+    public float yinc;
     public GameObject projectile;
     public Transform spawn;
     private Rigidbody2D m_rigidbody;
@@ -25,17 +26,23 @@ public class Cannon : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Rotation();
-        if(hiddenTimer >= projectileSpawnTimer)
+        if (hiddenTimer >= projectileSpawnTimer)
         {
+            if ((Mathf.Abs(playerObject.position.x) - Mathf.Abs(transform.position.x)) < 15|| (Mathf.Abs(playerObject.position.y) - Mathf.Abs(transform.position.y)) < 15)
+            { 
             hiddenTimer = 0f;
             GameObject obj = projectile;
             Vector2 direction = playerObject.position - transform.position;
+            direction.Normalize();
+            direction = new Vector2(direction.x, direction.y + yinc);
             direction *= force;
-            direction = new Vector2(direction.x, Mathf.Abs(direction.y * 25f));
-            obj.GetComponent<Rigidbody2D>().velocity = direction;
-            GameObject newObj = (GameObject)Instantiate(obj,spawn);
+
+
+            GameObject newObj = (GameObject)Instantiate(obj, spawn);
+            newObj.GetComponent<Rigidbody2D>().velocity = direction;
             newObj.transform.parent = null;
             Destroy(newObj, 4f);
+            }
         }
     }
 
@@ -44,7 +51,8 @@ public class Cannon : MonoBehaviour {
         hiddenTimer += Time.deltaTime;
         Vector2 direction = playerObject.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        if (angle > 240 || angle < -60 || (angle > 240 && angle < 300))
+        //Debug.Log(angle);
+        if ((angle > 260 && angle < 300))
             return;
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotationSpeed);
---
+++ Assets/applyVelocity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class applyVelocity : MonoBehaviour {
+
+    public float mult = 3f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<Rigidbody2D>())
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity * mult;
+        }
+    }
+}
